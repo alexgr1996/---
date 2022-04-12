3a68d0175f40ae841a938307f20b7109cd48c5bd
alexgr1996 <101583777+alexgr1996@users.noreply.github.com>
2022-04-12 21:43:02 +0300

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -7,16 +7,17 @@ create table Users(
 
 create table Quiz_Executions(
  	ExecId int AUTO_INCREMENT PRIMARY KEY,
-	 UserId int FOREIGN KEY REFERENCES Users(UserId),
-  AsID int FOREIGN KEY REFERENCES Assessments(AsID),
-	 quiz_date timestamp,
-	 quiz_sxore int not null
-	 
+	UserId int,
+  	AsID int,
+	quiz_date timestamp,
+	quiz_sxore int not null,
+	FOREIGN KEY (UserId) REFERENCES Users(UserId),
+	FOREIGN KEY (AsID) REFERENCES Assessments(AsID) 
 );
 
 CREATE TABLE Assessments (
   AsID INT AUTO_INCREMENT PRIMARY KEY,
-  ExecId int FOREIGN KEY REFERENCES Quiz_excecutions(ExecId),
+  ExecId int ,
   Assessment_Category VARCHAR(255) NOT NULL,
   Assessment_Difficulty INT NOT NULL,
   Assessment_Creator VARCHAR(255) NOT NULL ,
@@ -24,6 +25,7 @@ CREATE TABLE Assessments (
   QID VARCHAR(255) NOT NULL,
   CoAnID VARCHAR(255) NOT NULL,  /*check  for the function if needed to be able to have more than 1 correct anwser */
   WrAID VARCHAR(255) NOT NULL
+  FOREIGN KEY (ExecId) REFERENCES Quiz_excecutions(ExecId)
   
  
  );
