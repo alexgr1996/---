0061c063c4764fb3ceecbaf8283167ae62dd8a0b
george kalleas <georgekalleas@outlook.com>
2022-04-20 22:05:54 +0300

    [squize] feat/db: adding tables

--- Schema.sql
+++ Schema.sql
@@ -26,6 +26,67 @@ CREATE TABLE Assessments (
   CoAnID VARCHAR(255) NOT NULL,  /*check  for the function if needed to be able to have more than 1 correct anwser */
   WrAID VARCHAR(255) NOT NULL,
   FOREIGN KEY (ExecId) REFERENCES Quiz_excecutions(ExecId)
-  
  
  );
+
+CREATE TABLE categories
+(
+    description character varying(200) COLLATE pg_catalog."default",
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    CONSTRAINT categories_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE tags
+(
+    description character varying(200) COLLATE pg_catalog."default",
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    CONSTRAINT tags_pkey PRIMARY KEY (id)
+)
+
+CREATE TABLE questions
+(
+    difficulty character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    host character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    text character varying(200) COLLATE pg_catalog."default",
+    id integer NOT NULL,
+    correct_choice_pos smallint NOT NULL,
+    category_id integer NOT NULL,
+    CONSTRAINT questions_pkey PRIMARY KEY (id),
+    CONSTRAINT que_cat_fk FOREIGN KEY (category_id)
+        REFERENCES categories (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+CREATE TABLE question_choices
+(
+    text character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    que_id integer NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    "position" smallint NOT NULL,
+    CONSTRAINT question_choices_pkey PRIMARY KEY (id),
+    CONSTRAINT que_ch_que_fk FOREIGN KEY (que_id)
+        REFERENCES questions (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+
+CREATE TABLE assesment_questions
+(
+    que_id integer NOT NULL,
+    ass_id integer NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    CONSTRAINT assesment_questions_pkey PRIMARY KEY (id),
+    CONSTRAINT ass_q_ass_fk FOREIGN KEY (ass_id)
+        REFERENCES assesment (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT ass_q_que_fk FOREIGN KEY (que_id)
+        REFERENCES questions (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
