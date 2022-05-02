0881d5c6e6a0e61db70c1a868b181f80c608b8f9
george kalleas <georgekalleas@outlook.com>
2022-05-02 19:41:33 +0300

    [squize] feat/db: create tables

--- Schema.sql
+++ Schema.sql
@@ -15,78 +15,63 @@ create table Quiz_Executions(
 	FOREIGN KEY (AsID) REFERENCES Assessments(AsID) 
 );
 
-CREATE TABLE Assessments (
-  AsID INT AUTO_INCREMENT PRIMARY KEY,
-  ExecId int ,
-  Assessment_Category VARCHAR(255) NOT NULL,
-  Assessment_Difficulty INT NOT NULL,
-  Assessment_Creator VARCHAR(255) NOT NULL ,
-  Assessment_Title VARCHAR(255), 
-  QID VARCHAR(255) NOT NULL,
-  CoAnID VARCHAR(255) NOT NULL,  /*check  for the function if needed to be able to have more than 1 correct anwser */
-  WrAID VARCHAR(255) NOT NULL,
-  FOREIGN KEY (ExecId) REFERENCES Quiz_excecutions(ExecId)
- 
- );
+CREATE TABLE IF NOT EXISTS `categories` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `description` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE categories
-(
-    description character varying(200) COLLATE pg_catalog."default",
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    CONSTRAINT categories_pkey PRIMARY KEY (id)
-)
 
-CREATE TABLE tags
-(
-    description character varying(200) COLLATE pg_catalog."default",
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    CONSTRAINT tags_pkey PRIMARY KEY (id)
-)
+CREATE TABLE IF NOT EXISTS `questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `difficulty` smallint(6) NOT NULL DEFAULT 1,
+  `question_text` varchar(4000) COLLATE utf8_unicode_ci NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `correct_choise_pos` smallint(6) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `que_cate_fk` (`category_id`),
+  CONSTRAINT `que_cate_fk` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE questions
-(
-    difficulty character varying(200) COLLATE pg_catalog."default" NOT NULL,
-    host character varying(200) COLLATE pg_catalog."default" NOT NULL,
-    text character varying(200) COLLATE pg_catalog."default",
-    id integer NOT NULL,
-    correct_choice_pos smallint NOT NULL,
-    category_id integer NOT NULL,
-    CONSTRAINT questions_pkey PRIMARY KEY (id),
-    CONSTRAINT que_cat_fk FOREIGN KEY (category_id)
-        REFERENCES categories (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
 
-CREATE TABLE question_choices
-(
-    text character varying(200) COLLATE pg_catalog."default" NOT NULL,
-    que_id integer NOT NULL,
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    "position" smallint NOT NULL,
-    CONSTRAINT question_choices_pkey PRIMARY KEY (id),
-    CONSTRAINT que_ch_que_fk FOREIGN KEY (que_id)
-        REFERENCES questions (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
+CREATE TABLE IF NOT EXISTS `question_choices` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `text` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `que_id` int(11) NOT NULL,
+  `position` smallint(6) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`id`),
+  KEY `que_sc_que_fk` (`que_id`),
+  CONSTRAINT `que_sc_que_fk` FOREIGN KEY (`que_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE assesment_questions
-(
-    que_id integer NOT NULL,
-    ass_id integer NOT NULL,
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
-    CONSTRAINT assesment_questions_pkey PRIMARY KEY (id),
-    CONSTRAINT ass_q_ass_fk FOREIGN KEY (ass_id)
-        REFERENCES assesment (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID,
-    CONSTRAINT ass_q_que_fk FOREIGN KEY (que_id)
-        REFERENCES questions (id) MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
-)
+
+
+CREATE TABLE IF NOT EXISTS `assesment` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ass_cat_fk` (`category_id`),
+  CONSTRAINT `ass_cat_fk` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+CREATE TABLE IF NOT EXISTS `assestment_questions` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ass_id` int(11) NOT NULL,
+  `ques_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `asse_que_asse_fk` (`ass_id`),
+  KEY `asse_que_que_fk` (`ques_id`),
+  CONSTRAINT `asse_que_asse_fk` FOREIGN KEY (`ass_id`) REFERENCES `assesment` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `asse_que_que_fk` FOREIGN KEY (`ques_id`) REFERENCES `questions` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+
+
+
+-- Data exporting was unselected.
+
+-- Dumping structure for πίνακας squize.question_choices
