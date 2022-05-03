2e41b3bd986746079ff08965673ca727a1c20a75
alexgr1996 <101583777+alexgr1996@users.noreply.github.com>
2022-05-03 22:36:55 +0300

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -11,7 +11,8 @@ create table codes(
 	id BIGINT AUTO_INCREMENT PRIMARY KEY,
 	email varchar(100),
 	code varchar(5),
-	expire int(11)
+	expire int(11),
+	FOREIGN KEY (email) REFERENCES users(user_mail)
 
 );
 
