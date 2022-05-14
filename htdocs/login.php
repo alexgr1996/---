146fa5e3c8f8f0a299f3c9233e980cc08fa03837
dirollo13 <59145791+dirollo13@users.noreply.github.com>
2022-05-14 15:02:06 +0300

    css for box login and singup

--- htdocs/login.php
+++ htdocs/login.php
@@ -48,6 +48,13 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8"> -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/bootstrap.css">
+  <link rel="stylesheet" href="../css/our.css">
+  <!-- <script type="text/javascript" src="../js/javascript.js"> --> -->
+
 	<title>Login</title>
 </head>
 <body>
--- htdocs/signup.php
+++ htdocs/signup.php
@@ -57,6 +57,10 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="../css/bootstrap.css">
 	<title>Signup</title>
 </head>
 <body>
