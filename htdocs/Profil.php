41d15d7efd62b83ae5ef18326b112ce1c9987e77
Nikolaos987 <73761101+Nikolaos987@users.noreply.github.com>
2022-05-27 00:16:41 +0300

    profil changes

--- htdocs/Profil.php
+++ htdocs/Profil.php
@@ -18,65 +18,86 @@ session_start();
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
+    <!-- MDB -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/4.1.0/mdb.min.css" rel="stylesheet"/>
+
     <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="../css/our.css"> 
     <link rel="stylesheet" href="../css/test.css">
     <link rel="stylesheet" href="../css/info.css">
 
-    <script type="text/javascript" src="../js/javascript.js">
-
-    </script>
+    <script type="text/javascript" src="../js/javascript.js"> </script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+    
     <title>Profile Menu</title>
   </head>
   <body>
   
-    <div class="WebContainer">
-        <div class="header">
-            <header>
-                <img class="logo circular--square" src="../html/lg.jpg" alt="logo">
-                <nav>
-                <ul class="nav_links">
-                    <li><a href="ChooseTest.php">Home</a></li>
-                    <li><a href="Profil.php">Profil</a></li>
-                </ul>
-                </nav>
-                <a class="logoutButton" href="login.php"><button>Logout</button></a>
-            </header>
+
+
+  <header >
+      <img class="logo circular--square" src="../html/lg.jpg" alt="logo">
+      <nav>
+        <ol class="nav_links" >
+          <li><a href="ChooseTest.php"><button> Home Page </button></a></li>
+          <li><a href="profile.html"><button> Profile </button></a></li>
+          <li><a href="#"><button> Contact us </button></a></li>
+          <li><a class="logoutButton" href="welcome.html"><button>Logout</button></a></li>
+        </ol>
+      </nav>
+    </header>
+
+
+    <div class="container2" >
+      <div class="row d-flex justify-content-center">
+        <div class="col-md-8 mt-1 ">
+         <div class="row z-depth-3">   
+
+                <div class="col-sm-15  ">
+                        <div class=" text-center text-white">
+                          <!-- <i class="mt-5"></i>  -->
+                            <h2 class="text-center font-weight-bold mt-3"></h2>
+                            <p></p>
+                            <!--  <i class="far fa-edit fa-2x mb-4"></i>    -->
+                        </div>
+                </div><br>
+      
+                        <!-- <div class="row"> -->
+                        <div class="container1">
+                            <h2>
+                              <span>Information</span> 
+                            </h2>            
+                            <table>
+                                <tr>
+                                    <th>USERNAME</th>
+                                    <td><?php echo $user_data['user_name']; ?></td>
+                                </tr>
+
+                                <tr>
+                                    <th>ID</th>
+                                    <td><?php echo $user_data['user_id']; ?></td>
+                                </tr>
+
+                                <tr>
+                                    <th>EMAIL</th>
+                                    <td><?php echo $user_data['user_mail']; ?></td>
+                                </tr>
+
+                                <tr>
+                                    <th>DATE</th>
+                                    <td><?php echo $user_data['date']; ?></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div class="col-md-8 mt-5 pt-2"></div>
+
+           </div>
         </div>
-
-        <div class="container">
-        <h2>
-            ACCOUNT INFORMATION
-        </h2>
-
-        <table>
-            <tr>
-                <th>ID</th>
-                <td><?php echo $user_data['user_id']; ?></td>
-            </tr>
-
-            <tr>
-                <th>USERNAME</th>
-                <td><?php echo $user_data['user_name']; ?></td>
-            </tr>
-
-            <tr>
-                <th>EMAIL</th>
-                <td><?php echo $user_data['user_mail']; ?></td>
-            </tr>
-
-            <tr>
-                <th>DATE</th>
-                <td><?php echo $user_data['date']; ?></td>
-            </tr>
-        </table>
-        </div>
+      </div>
     </div>
 
-</body>
-</html>
