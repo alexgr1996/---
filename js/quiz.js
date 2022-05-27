f2e5b695826b20fad38ba9a0979a98d0eeb3c11d
JojoHojo <georgiapella@hotmail.com>
2022-05-27 12:20:46 +0300

    s

--- js/quiz.js
+++ js/quiz.js
@@ -184,7 +184,7 @@ countdown.innerHTML = duration;
 
 
 
-/* const start_btn = document.querySelector(".start_btn button");  */
+/*// const start_btn = document.querySelector(".start_btn button");  
 const info_box = document.querySelector(".info_box");
 /*const exit_btn = info_box.querySelector(".buttons .quit");  
 const continue_btn = info_box.querySelector(".buttons .restart");
